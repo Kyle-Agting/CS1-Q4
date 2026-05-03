43c29f05cbb46213414fc17085f3caf8348fa528
AndreiEderango <abederango@cmc.pshs.edu.ph>
2026-05-03 08:44:29 +0800

    the thing on the bottom is wrong

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ quarter_1st= Q1
 quarter_2nd = quarter_1st + (2 * Q2) / 3
 quarter_3rd = quarter_2nd + (2 * Q3) / 3
 quarter_4th = quarter_3rd + (2 * Q4) / 3
-print("1st Quarter Grade: ", Q1)
-print("2nd Quarter Grade: ", Q2)
-print("3rd Quarter Grade: ", Q3)
-print("4th Quarter Grade: ", Q4)
+print("1st Quarter Grade: ", quarter_1st)
+print("2nd Quarter Grade: ", quarter_2nd)
+print("3rd Quarter Grade: ", quarter_3rd)
+print("4th Quarter Grade: ", quarter_4th)
