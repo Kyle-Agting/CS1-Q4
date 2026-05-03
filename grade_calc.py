5e841b279a38202c0d70f90954b9012eb4c256f8
AndreiEderango <abederango@cmc.pshs.edu.ph>
2026-05-03 08:54:28 +0800

    I did something that could make it more usable ig

--- grade_calc.py
+++ grade_calc.py
@@ -4,29 +4,29 @@ Q3 = float(input(“Q3 Tentative: ”))
 Q4 = float(input(“Q4 Tentative: ”))
 
 # Cumulative Calculations
-q1 = Q1 
-q2 = (q1 + 2 * Q2) / 3
-q3 = (q2 + 2 * Q3) / 3
-q4 = (q3 + 2 * Q4) / 3
+quarter_1st = Q1 
+quarter_2nd = (quarter_1st + (2 * Q2)) / 3
+quarter_3rd = (quarter_2nd + (2 * Q3)) / 3
+quarter_4th = (quarter_3rd + (2 * Q4)) / 3
 
 # Optimized Printing and Status
-print("1st Quarter Grade: ", q1)
-print("2nd Quarter Grade: ", q2)
-print("3rd Quarter Grade: ", q3)
-print("4th Quarter Grade: ", q4)
+print("1st Quarter Grade: ", quarter_1st)
+print("2nd Quarter Grade: ", quarter_2nd)
+print("3rd Quarter Grade: ", quarter_3rd)
+print("4th Quarter Grade: ", quarter_4th)
 
 # Adjectival logic for the Final Grade (q4)
-if q4 >= 96:
+if quarter_4th >= 96 and quarter_4th == 100:
     print("Status: EXCELLENT")
-elif q4 >= 84:
+elif quarter_4th >= 84 and quarter_4th == 95.99:
     print("Status: VERY GOOD")
-elif q4 >= 72:
+elif quarter_4th >= 72 and quarter_4th == 83.99:
     print("Status: GOOD")
-elif q4 >= 60:
+elif quarter_4th >= 60 and quarter_4th == 71.99:
     print("Status: SATISFACTORY")
-elif q4 >= 50:
+elif quarter_4th >= 50 and quarter_4th == 59.99:
     print("Status: FAIR")
-elif q4 >= 40:
+elif quarter_4th >= 40 and quarter_4th == 49.99:
     print("Status: FAILED ON CONDITION")
 else:
     print("Status: FAILED")
